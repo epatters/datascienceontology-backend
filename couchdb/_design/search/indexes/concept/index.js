2a285dbedbe28423d488ceeaec23e6ae4e0724ed
epatters <ejpatters@gmail.com>
2017-07-13 11:30:23 -0400

    Add default field to concept search index.

--- couchdb/_design/search/indexes/concept/index.js
+++ couchdb/_design/search/indexes/concept/index.js
@@ -1,5 +1,8 @@
 function(doc) {
   if (doc.schema === "concept") {
+    // If no field specified, search concept ID, name, and description.
+    index("default", [doc.id, doc.name, doc.description].join(" "));
+    
     index("ontology", doc.ontology, {"store": true});
     index("id", doc.id, {"store": true});
     if (doc.name) {
