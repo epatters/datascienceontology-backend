beb2693e052c9bb7c7516521052276a16b1b61c9
epatters <ejpatters@gmail.com>
2017-07-19 11:30:33 -0400

    Graphviz action is now working!

---
+++ openwhisk/docker/graphviz/action.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python 
+
+import json
+import subprocess
+import sys
+
+ENGINES = [
+    "dot",
+    "neato",
+    "sfdp"
+    "fdp",
+    "twopi",
+    "circo",
+]
+
+FORMATS = [
+    "dot",
+    "gv",
+    "xdot",
+    "json",
+    "json0",
+    "dot_json",
+    "dot_json0",
+    "svg",
+]
+
+
+def main(argv):
+    # Parse JSON and action parameters.
+    params = json.loads(argv[1])
+    data = params["data"]
+    engine = params.get("engine", "dot")
+    assert engine in ENGINES
+    fmt = params.get("format", "json0")
+    assert fmt in FORMATS
+    
+    # Run Graphviz!
+    ran = subprocess.run([engine, "-T"+fmt], input=data, encoding="utf-8",
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+    # Build the response and print to stdout.
+    if ran.returncode == 0:
+        data = json.loads(ran.stdout) if "json" in fmt else ran.stdout
+        result = {
+            "success": True,
+            "data": data,
+            "format": fmt,
+        }
+    else:
+        result = {
+            "success": False,
+            "stdout": ran.stdout,
+            "stderr": ran.stderr,
+        }
+    json.dump(result, sys.stdout)
+    
+
+if __name__ == '__main__':
+    main(sys.argv)
