03a06bf173e40a6c475071006984f8b2e0232198
epatters <ejpatters@gmail.com>
2017-07-20 09:27:44 -0400

    Stub out Julia action for converting expressions to Graphviz.
    The expression is hard-coded! I need to decide how expressions and
    presentations will be serialized.

---
+++ openwhisk/catlab/action.jl
@@ -0,0 +1,43 @@
+#!/usr/bin/env julia
+import JSON
+
+using Catlab.Doctrine
+import Catlab.Diagram: Graphviz
+using Catlab.Diagram: Wiring, GraphvizWiring
+
+
+function expression_to_graphviz()::Graphviz.Graph
+  # XXX: Hard-code an expression for now!
+  A, B, C = ob(FreeCartesianCategory, :A, :B, :C)
+  f, g, h = hom(:f, A, B), hom(:g, B, C), hom(:h, A, C)
+  expr = otimes(compose(f, g), h)
+  to_graphviz(to_wiring_diagram(expr))
+end
+
+
+function main(args::Vector{String})
+  # Parse JSON and action parameters.
+  params = JSON.parse(args[1])
+  action = params["action"]
+  
+  # Run action!
+  result = if action == "expression_to_graphviz"
+    graph = expression_to_graphviz()
+    dot = sprint(Graphviz.pprint, graph)
+    Dict(
+      "success" => true,
+      "data" => dot,
+      "mimetype" => "text/vnd.graphviz",
+    )
+  else
+    Dict(
+      "success" => false,
+      "error" => "Unknown subaction: $action",
+    )
+  end
+  
+  # Print result JSON to stdout.
+  JSON.print(result)
+end
+
+main(ARGS)
--- openwhisk/install.sh
+++ openwhisk/install.sh
@@ -18,3 +18,7 @@ wsk action update --web true $PKG/graphviz \
 wsk action update --web true $PKG/graphviz_to_cytoscape \
   --sequence $PKG/graphviz,$PKG/dot_to_cytoscape \
   -a description "Run Graphviz and convert output to Cytoscape data"
+
+wsk action update $PKG/catlab \
+  --docker $DOCKER/whisk-catlab \
+  -a description "Run a subaction in Catlab"
