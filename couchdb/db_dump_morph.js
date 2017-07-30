330a034ba92481bbb2db19c0c6c54355a644e83a
epatters <ejpatters@gmail.com>
2017-07-30 15:39:53 -0400

    Skip cache documents in database dump.

--- couchdb/db_dump_morph.js
+++ couchdb/db_dump_morph.js
@@ -3,5 +3,9 @@ module.exports = function(doc, cb) {
     // Omit design documents since they contain large JS blobs.
     return cb();
   }
+  if (doc._id.startsWith("cache/")) {
+    // Omit transient "cache" documents.
+    return cb();
+  }
   return cb(null, doc);
 }
