3762ed736a0f3900d5262cfc50e72a482cbf5ee8
epatters <ejpatters@gmail.com>
2017-08-15 15:34:52 -0400

    Add install script for CouchDB.

---
+++ couchdb/install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+pushd() { builtin pushd "$@" > /dev/null; }
+popd() { builtin pushd "$@" > /dev/null; }
+
+echo "Pushing design document: schema"
+pushd _design/schema
+couchapp push
+popd
+
+echo "Pushing design document: query"
+pushd _design/query
+couchapp push
+popd
+
+echo "Pushing design document: search"
+pushd _design/search
+couchapp push
+popd
+
+echo "Creating bulk data"
+jq '{docs: .}' ../../ontology.json |
+  ccurl -X POST -d @- /data-science-ontology/_bulk_docs |
+  jq '.'
