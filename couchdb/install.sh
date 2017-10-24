3e1ab3e476831f16dbd9fb92838f378f7064c8e7
epatters <ejpatters@gmail.com>
2017-10-23 19:09:48 -0500

    Don't use default environment for Couchapp.

--- couchdb/install.sh
+++ couchdb/install.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
 set -e
 
+COUCHAPP="data-science-ontology"
+
 pushd() { builtin pushd "$@" > /dev/null; }
 popd() { builtin pushd "$@" > /dev/null; }
 
 echo "Pushing design document: schema"
 pushd _design/schema
-couchapp push
+couchapp push $COUCHAPP
 popd
 
 echo "Pushing design document: query"
 pushd _design/query
-couchapp push
+couchapp push $COUCHAPP
 popd
 
 echo "Pushing design document: search"
 pushd _design/search
-couchapp push
+couchapp push $COUCHAPP
 popd
 
 echo "Creating bulk data"
