994b30cd88a67e1b4d9d4f985a0ebf0fc09c3098
epatters <ejpatters@gmail.com>
2017-10-27 19:46:39 -0500

    Work around Composer bug in action to update cached data.
    Message from Olivier Tardieu:
    
    composing a named app inside of an app will be possible soon
    the code is here
    https://github.com/tardieu/composer/tree/parallel
    but it has not being merged into master yet or the shell
    for the time being this will require naming apps with a “.app” suffix
    we need to push some changes to bluemix to be able to “follow the
    session id” without knowing in advance that an app is being
    invoked
    for now you can play one trick for an app that completes in 5 minutes
    or less
    add a $blocking = true param to the app invocation

--- openwhisk/composer/update_morphism_annotation_cache.js
+++ openwhisk/composer/update_morphism_annotation_cache.js
@@ -1,6 +1,11 @@
 composer.sequence(
   args => Object.assign(args, {
-    dbname: "data-science-ontology-webapp"
+    dbname: "data-science-ontology-webapp",
+    /* Work around Composer bug where long-running apps return a session ID
+       rather than the actual result. When the bug is fixed the `$blocking`
+       argument can be removed.
+     */
+    $blocking: true,
   }),
   composer.task("Bluemix_Cloudant_Root/read", { output: "doc" }),
   composer.task("open-discovery/annotation_to_cytoscape", { output: "graphs" }),
