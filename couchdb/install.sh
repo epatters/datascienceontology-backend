cef150f71c4046362722223fd9c38e7a2b623a11
epatters <ejpatters@gmail.com>
2018-02-20 20:14:44 -0600

    Move schema index to schema design doc and delete annotation index.

--- couchdb/install.sh
+++ couchdb/install.sh
@@ -11,11 +11,6 @@ pushd _design/schema
 couchapp push $COUCHAPP
 popd
 
-echo "Pushing design document: query"
-pushd _design/query
-couchapp push $COUCHAPP
-popd
-
 echo "Pushing design document: search"
 pushd _design/search
 couchapp push $COUCHAPP
