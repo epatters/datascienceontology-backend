7bcfab63e45fd728339a624bdc514ad5cd0ac397
epatters <ejpatters@gmail.com>
2018-08-02 20:32:15 -0700

    Implement API methods to search for concepts/annotations.

--- api/src/methods.ts
+++ api/src/methods.ts
@@ -27,7 +27,7 @@ function find(options: object) {
     method: 'POST',
     json: true,
     body: options,
-  }).then(result => result.docs) as Promise<Array<any>>;
+  }).then(result => result.docs as any[]);
 }
 
 function search(ddoc: string, index: string, options: object) {
@@ -36,6 +36,9 @@ function search(ddoc: string, index: string, options: object) {
     method: 'POST',
     json: true,
     body: options,
+  }).then(result => {
+    let rows = result.rows as { id: string, fields: object }[];
+    return rows.map(row => _.assign({_id: row.id}, row.fields));
   });
 }
 
@@ -44,6 +47,7 @@ function search(ddoc: string, index: string, options: object) {
 export function getConcept(id: string) {
   return get(`concept/${id}`);
 }
+
 export function getAnnotation(lang: string, pkg: string, id: string) {
   return get(`annotation/${lang}/${pkg}/${id}`);
 }
@@ -58,6 +62,7 @@ export function randomConcept() {
     }).then(docs => docs[0]);
   });
 }
+
 export function randomAnnotation() {
   return countsJSON().then(counts => {
     const nannations = counts.annotation;
@@ -69,21 +74,38 @@ export function randomAnnotation() {
   });
 }
 
+export function searchConcepts(text: string) {
+  const query = [
+    `id:(${text})^100`,      // Exact match on ID due to `keyword` analyzer
+    `name:(${text})^3`,      // Inexact match on name
+    `description:(${text})`, // Inexact match on description
+  ].join(' ');
+  return search('search', 'concept', { query });
+}
+
+export function searchAnnotations(text: string) {
+  const query = [
+    `language:(${text})`, `package:(${text})^3`, `id:(${text})^100`,
+    `name:(${text})^3`, `description:(${text})`,
+    `class:(${text})`, `function:(${text})`, `method:(${text})`
+  ].join(' ');
+  return search('search', 'annotation', { query });
+}
+
 export function counts() {
   return countsJSON().then(result => JSON.stringify(result));
 }
 function countsJSON(): Promise<{[key: string]: number}> {
-  return view("query", "schema_index", {
+  return view('query', 'schema_index', {
     group: true,
     reduce: true,
   }).then(body => {
     const result = JSON.parse(body) as {
       rows: { key: string[], value: number }[],
     };
-    return(_.chain(result.rows)
+    return _.chain(result.rows)
       .keyBy(row => row.key[0])
       .mapValues(row => row.value)
-      .value()
-    );
+      .value();
   });
 }
